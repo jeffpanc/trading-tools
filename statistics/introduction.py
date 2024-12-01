448d8690b4492819a59ffe5c2c4e436d4a5a129a
Jeff Pancottine <jeffpanc@icloud.com>
2024-12-01 15:46:50 -0800

    Update introduction.py

--- statistics/introduction.py
+++ statistics/introduction.py
@@ -28,39 +28,39 @@ st.markdown(
         - Monthly Returns Heatmap
         - Monthly Active Returns Heatmap
     * Level 1: Trade Statistics
-        Number of Trades
-        Wins
-        Percent Wins
-        Win/Loss Ratio
-        Average Trade Profit
-        Maximum Win and Loss
-        Maximum Win an Loss Streaks
-        Profit Factor
-        Expectancy
-        Trade Returns
+        - Number of Trades
+        - Wins
+        - Percent Wins
+        - Win/Loss Ratio
+        - Average Trade Profit
+        - Maximum Win and Loss
+        - Maximum Win an Loss Streaks
+        - Profit Factor
+        - Expectancy
+        - Trade Returns
     * Level 2: System Statistics
-        Month and Year Returns Arithmetic Mean
-        Month and Year Returns Geometric Mean
-        Monthly and Yearly Volatility
-        Downside Deveation
-        Beta
-        Annual Alpha
-        R-Squared
-        MAR Ratio
-        Treynor Ratio
-        3-Year Calmar Ratio
-        Yearly Active Returns
-        Tracking Error
-        Information Ratio
-        Skewness
-        Kurtosis
-        Analytical VaR
-        Historical VaR
-        Conditional VaR
-        Upside Capture Ratio
-        Downside Capture Ratio
-        Total Capture Ratio
-        Top 5 Drawdowns Portfolio and Benchmark
+        - Month and Year Returns Arithmetic Mean
+        - Month and Year Returns Geometric Mean
+        - Monthly and Yearly Volatility
+        - Downside Deveation
+        - Beta
+        - Annual Alpha
+        - R-Squared
+        - MAR Ratio
+        - Treynor Ratio
+        - 3-Year Calmar Ratio
+        - Yearly Active Returns
+        - Tracking Error
+        - Information Ratio
+        - Skewness
+        - Kurtosis
+        - Analytical VaR
+        - Historical VaR
+        - Conditional VaR
+        - Upside Capture Ratio
+        - Downside Capture Ratio
+        - Total Capture Ratio
+        - Top 5 Drawdowns Portfolio and Benchmark
     * Level 3: All Statistics
     
 """

dacd0cb4ff2426c07b28373730934a36de182032
Jeff Pancottine <jeffpanc@icloud.com>
2025-02-17 14:46:59 -0800

    plot update

--- algo_tools/stats.py
+++ algo_tools/stats.py
@@ -65,151 +65,159 @@ with st.form('User Input'):
         bmark.set_index('date', drop=True, inplace=True)
         bmark.index = pd.to_datetime(bmark.index)
 
-        reports_lib.reports_output(selected_level, account, trades, bmark, selected_bmark, account.index[0], account.index[-1], selected_periodicity, selected_Rf)
+        if selected_level == 0 or selected_level == 1 or selected_level == 2 or selected_level == 3:       # always print summary
+            # create active returns for heatmap
+            active_df = pd.DataFrame()
+            active_df['returns'] = account['returns'] - bmark['returns']
+            active_df.index = account.index
 
+            st.write('')
+            st.write("__Performance Summary:__", f"{account.index[0]:%B %d, %Y}" , "  ", "to","  ", f"{account.index[-1]:%B %d, %Y}")
+            st.write('')
 
-        # if selected_level == 0 or selected_level == 1 or selected_level == 2 or selected_level == 3:       # always print summary
-        #     # create active returns for heatmap
-        #     active_df = pd.DataFrame()
-        #     active_df['returns'] = account['returns'] - bmark['returns']
-        #     active_df.index = account.index
+            # L0 stats table
+            L0 = reports_lib.make_L0_metrics(account, bmark, selected_bmark, selected_periodicity, selected_Rf)
+            st.dataframe(L0)
+            st.write('')
 
-        #     st.write('')
-        #     st.write("__Performance Summary:__", f"{account.index[0]:%B %d, %Y}" , "  ", "to","  ", f"{account.index[-1]:%B %d, %Y}")
-        #     st.write('')
-
-        #     # L0 stats table
-        #     L0 = reports_lib.make_L0_metrics(account, bmark, selected_bmark, selected_periodicity, selected_Rf)
-        #     st.dataframe(L0)
-        #     st.write('')
-
-        #     # Equity curves
-        #     fig = px.line(account,  x=account.index, y=['balance', bmark['balance']], title="Equity Curves (Log Scale)", labels={'value': 'US$', 'index':'Year','variable': ''}, log_y=True, color_discrete_map={
-        #          "balance": "blue",
-        #          "bmark['balance']": "orange"
-        #      }) 
-        #     newnames = {'balance':'Model', 'wide_variable_1': selected_bmark}
-        #     fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
-        #                               legendgroup = newnames[t.name],
-        #                               hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])))
-        #     st.plotly_chart(fig)
-        #     st.write('')
+            # Equity curves
+            fig = px.line(account,  x=account.index, y=['balance', bmark['balance']], title="Equity Curves (Log Scale)", labels={'value': 'US$', 'index':'Year','variable': ''}, log_y=True, color_discrete_map={
+                 "balance": "blue",
+                 "bmark['balance']": "orange"
+             }) 
+            newnames = {'balance':'Model', 'wide_variable_1': selected_bmark}
+            fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
+                                      legendgroup = newnames[t.name],
+                                      hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])))
+            st.plotly_chart(fig)
+            st.write('')
     
-        #     # Drawdown curves
-        #     fig = px.line(account,  x=account.index, y=['DD', bmark['DD']], title="Drawdown Curves", labels={'value': 'Percent', 'index':'Year', 'variable': ''}, color_discrete_map={
-        #          "DD": "blue",
-        #          "bmark['DD']": "orange"
-        #      } )
-        #     newnames = {'DD':'Model', 'wide_variable_1': selected_bmark}
-        #     fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
-        #                               legendgroup = newnames[t.name],
-        #                               hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])))
-        #     st.plotly_chart(fig)
-        #     st.write('')
+            # Drawdown curves
+            fig = px.line(account,  x=account.index, y=['DD', bmark['DD']], title="Drawdown Curves", labels={'value': 'Percent', 'index':'Year', 'variable': ''}, color_discrete_map={
+                 "DD": "blue",
+                 "bmark['DD']": "orange"
+             } )
+            newnames = {'DD':'Model', 'wide_variable_1': selected_bmark}
+            fig.for_each_trace(lambda t: t.update(name = newnames[t.name],
+                                      legendgroup = newnames[t.name],
+                                      hovertemplate = t.hovertemplate.replace(t.name, newnames[t.name])))
+            st.plotly_chart(fig)
+            st.write('')
 
-        #     # Account returns
-        #     fig = px.line(account['returns'],  x=account.index, y=['returns'], title="Account Returns (%)", labels={'value': 'Percent', 'index':'Year', 'variable': ''}, color_discrete_map={
-        #          "returns": "blue"
-        #      } )
-        #     st.plotly_chart(fig)
-        #     st.write('')
+            # Account returns
+            # Define colors based on y values
+            portfolio_pct_returns["color"] = portfolio_pct_returns.returns.apply(lambda val: "less then 0" if val < 0 else "greater than or equal 0")
+        
+            fig = px.bar(portfolio_pct_returns,  x=portfolio_pct_returns.index, y=['returns'], color=portfolio_pct_returns["color"], color_discrete_map={"less then 0": "red", "greater than or equal 0": "green"}, title="Portfolio Account Returns (%)",  labels={'value': 'Percent', 'index':'Year', 'variable': ''} )
+            fig.update_layout(legend_title_text="Account Return Value")
+            st.plotly_chart(fig)
+            st.write('')
 
-        #     # Account returns distribution
-        #     fig = px.histogram(account['returns'],  x=['returns'], title="Account Returns Distribution (%)", nbins=250, labels={'value': '% Return', 'variable': ''}, color_discrete_map={
-        #          "returns": "blue"
-        #      }  )
-        #     fig.update_layout(
-        #         yaxis_title_text='Count', # yaxis label
-        #     )        
-        #     st.plotly_chart(fig)
-        #     st.write('')
+            # Account returns distribution
+            # Define colors based on y values
+            portfolio_pct_returns["color"] = portfolio_pct_returns.returns.apply(lambda val: "less then 0" if val < 0 else "greater than or equal 0")
+        
+            fig = px.histogram(portfolio_pct_returns,  x=['returns'], title="Portfolio Account Returns Distribution (%)", color=portfolio_pct_returns["color"], color_discrete_map={"less then 0": "red", "greater than or equal 0": "green"},nbins=250, labels={'value': '% Return', 'variable': ''} )
+            
+            fig.update_layout(
+                yaxis_title_text='Count', # yaxis label
+                legend_title_text="Account Return Values"
+            )
+            st.plotly_chart(fig)
+            st.write('')
 
             
-        # if selected_level == 1 or selected_level == 3:
-        #     st.write('')
-        #     st.write("__Trade Metrics:__   " ,  f"{account.index[0]:%B %d, %Y}" , "  to  ", f"{account.index[-1]:%B %d, %Y}")
-        #     st.write('')
+        if selected_level == 1 or selected_level == 3:
+            st.write('')
+            st.write("__Trade Metrics:__   " ,  f"{account.index[0]:%B %d, %Y}" , "  to  ", f"{account.index[-1]:%B %d, %Y}")
+            st.write('')
 
-        #     L1 = reports_lib.make_L1_metrics(trades)
-        #     st.dataframe(L1)
+            L1 = reports_lib.make_L1_metrics(trades)
+            st.dataframe(L1)
 
-        #     # Trade returns
-        #     fig = px.line(trades['trade_ret_pct'],  x=trades.index, y=['trade_ret_pct'], title="Trade Returns (%)", labels={'value': 'Percent', 'index':'Trade Number', 'variable': ''}, color_discrete_map={
-        #          "trade_ret_pct": "blue"
-        #      }   )
-        #     st.plotly_chart(fig)
-        #     st.write('')
+            # Trade returns
+            # Define colors based on y values
+            trades_returns["color"] = trades_returns.trade_ret_pct.apply(lambda val: "less then 0" if val < 0 else "greater than or equal 0")
+            
+            # Create scatter plot
+            fig = px.scatter(trades_returns, x=trades_returns.index, y=['trade_ret_pct'], color=trades_returns["color"], color_discrete_map={"less then 0": "red", "greater than or equal 0": "green"},  title="Portfolio Trade Returns (%)", labels={'value': 'Percent', 'index':'Trade Number', 'variable': ''})
+            fig.update_layout(legend_title_text="Trade Return Value")
+            st.plotly_chart(fig)
+            st.write('')
        
-        #     # Trade returns distribution        
-        #     fig = px.histogram(trades['trade_ret_pct'],  x=['trade_ret_pct'], title="Trade Returns Distribution (%)", nbins=100, labels={'value': '% Return', 'variable': ''}, color_discrete_map={
-        #          "trade_ret_pct": "blue"
-        #      }   )            
-        #     fig.update_layout(
-        #         yaxis_title_text='Count', # yaxis label
-        #     )        
-        #     st.plotly_chart(fig)
-        #     st.write('')
+            # Trade returns distribution        
+            # Define colors based on y values
+            trades_returns["color"] = trades_returns.trade_ret_pct.apply(lambda val: "less then 0" if val < 0 else "greater than or equal 0")
+        
+            fig = px.histogram(trades_returns,  x=['trade_ret_pct'], title="Portfolio Trade Returns Distribution (%)", color=trades_returns["color"], color_discrete_map={"less then 0": "red", "greater than or equal 0": "green"},nbins=100, labels={'value': '% Return', 'variable': ''} )
+            
+            fig.update_layout(
+                yaxis_title_text='Count', # yaxis label
+                legend_title_text="Trade Returns Value"
+            )    
+            st.plotly_chart(fig)
+            st.write('')
 
         
-        # if selected_level == 2 or selected_level == 3:
-        #     st.write('')
-        #     st.write("__System Metrics:__   " ,  f"{account.index[0]:%B %d, %Y}" , "  to  ", f"{account.index[-1]:%B %d, %Y}")
-        #     st.write('')
+        if selected_level == 2 or selected_level == 3:
+            st.write('')
+            st.write("__System Metrics:__   " ,  f"{account.index[0]:%B %d, %Y}" , "  to  ", f"{account.index[-1]:%B %d, %Y}")
+            st.write('')
 
-        #     L2 = reports_lib.make_L2_metrics(account, bmark, selected_bmark, selected_periodicity, selected_Rf)
-        #     st.dataframe(L2)
+            L2 = reports_lib.make_L2_metrics(account, bmark, selected_bmark, selected_periodicity, selected_Rf)
+            st.dataframe(L2)
         
-        #     drawdowns_stats = []
-        #     start = 0
+            drawdowns_stats = []
+            start = 0
 
-        #     # Top 5 drawdowns
-        #     for i in range(1, len(account)):
-        #         if account.DD.iloc[i] < 0 and account.DD.iloc[i-1] == 0:               # find the peak
-        #             peak_date = account.index[i]
-        #             start = i
-        #         elif account.DD.iloc[i] == 0 and account.DD.iloc[i-1] < 0:             # find the recovery
-        #             record = account[account.DD == account.DD.iloc[start:i].min()]
-        #             valley_date = record.index[0]
-        #             recovery_date = account.index[i]
-        #             r1 = relativedelta.relativedelta(valley_date, peak_date) 
-        #             r2 = relativedelta.relativedelta(recovery_date, valley_date)
-        #             drawdowns_stats.append({
-        #                 'Drawdown': account.DD.iloc[start:i].min(),
-        #                 'Start Date': datetime.strftime(peak_date, '%Y-%m'),
-        #                 'End Date': datetime.strftime(valley_date, '%Y-%m'),
-        #                 'Drawdown (Months)': r1.months + 1 + (12 * r1.years),          # add 1 for current month
-        #                 'Recovery Date': datetime.strftime(recovery_date, '%Y-%m'),
-        #                 'Recovery (Months)': r2.months + (12 * r2.years),
-        #                 'Underwater (Months)': f"{(i - start + 1)/30:.2f}",
-        #             })
-        #             start = i
-        #         elif i == len(account) - 1 and account.DD.iloc[i] < 0:                 # handle ongoing drawdown
-        #             record2 = account[account.DD == account.DD.iloc[start:].min()]
-        #             valley_date = record2.index[0]
-        #             peak_date = account.index[start]
-        #             r3 = relativedelta.relativedelta(valley_date, peak_date)
-        #             drawdowns_stats.append({
-        #                 'Drawdown': account.DD.iloc[start:].min(),
-        #                 'Start Date': datetime.strftime(peak_date, '%Y-%m'),
-        #                 'End Date': datetime.strftime(valley_date, '%Y-%m'),
-        #                 'Drawdown (Months)': r3.months + 1 + (12 * r3.years),          # add 1 for current month
-        #                 'Recovery Date': 'On Going',       # Ongoing drawdown
-        #                 'Recovery (Months)': 'On Going',   # Ongoing drawdown
-        #                 'Underwater (Months)': 'On Going', # Ongoing drawdown
-        #             })
+            # Top 5 drawdowns
+            for i in range(1, len(account)):
+                if account.DD.iloc[i] < 0 and account.DD.iloc[i-1] == 0:               # find the peak
+                    peak_date = account.index[i]
+                    start = i
+                elif account.DD.iloc[i] == 0 and account.DD.iloc[i-1] < 0:             # find the recovery
+                    record = account[account.DD == account.DD.iloc[start:i].min()]
+                    valley_date = record.index[0]
+                    recovery_date = account.index[i]
+                    r1 = relativedelta.relativedelta(valley_date, peak_date) 
+                    r2 = relativedelta.relativedelta(recovery_date, valley_date)
+                    drawdowns_stats.append({
+                        'Drawdown': account.DD.iloc[start:i].min(),
+                        'Start Date': datetime.strftime(peak_date, '%Y-%m'),
+                        'End Date': datetime.strftime(valley_date, '%Y-%m'),
+                        'Drawdown (Months)': r1.months + 1 + (12 * r1.years),          # add 1 for current month
+                        'Recovery Date': datetime.strftime(recovery_date, '%Y-%m'),
+                        'Recovery (Months)': r2.months + (12 * r2.years),
+                        'Underwater (Months)': f"{(i - start + 1)/30:.2f}",
+                    })
+                    start = i
+                elif i == len(account) - 1 and account.DD.iloc[i] < 0:                 # handle ongoing drawdown
+                    record2 = account[account.DD == account.DD.iloc[start:].min()]
+                    valley_date = record2.index[0]
+                    peak_date = account.index[start]
+                    r3 = relativedelta.relativedelta(valley_date, peak_date)
+                    drawdowns_stats.append({
+                        'Drawdown': account.DD.iloc[start:].min(),
+                        'Start Date': datetime.strftime(peak_date, '%Y-%m'),
+                        'End Date': datetime.strftime(valley_date, '%Y-%m'),
+                        'Drawdown (Months)': r3.months + 1 + (12 * r3.years),          # add 1 for current month
+                        'Recovery Date': 'On Going',       # Ongoing drawdown
+                        'Recovery (Months)': 'On Going',   # Ongoing drawdown
+                        'Underwater (Months)': 'On Going', # Ongoing drawdown
+                    })
         
-        #     # Sort drawdowns by depth
-        #     drawdowns_stats = sorted(drawdowns_stats, key=lambda x: x['Drawdown'])[:5]
+            # Sort drawdowns by depth
+            drawdowns_stats = sorted(drawdowns_stats, key=lambda x: x['Drawdown'])[:5]
                
-        #     # Convert to DataFrame for easier viewing/manipulation
-        #     drawdowns_df = pd.DataFrame(drawdowns_stats)
-        #     drawdowns_df.index = drawdowns_df.index + 1
+            # Convert to DataFrame for easier viewing/manipulation
+            drawdowns_df = pd.DataFrame(drawdowns_stats)
+            drawdowns_df.index = drawdowns_df.index + 1
         
-        #     # Display the top 5 drawdowns
-        #     st.write('')
-        #     st.write('__Model Top 5 Drawdowns__')
-        #     st.write('')
-        #     st.dataframe(drawdowns_df)
+            # Display the top 5 drawdowns
+            st.write('')
+            st.write('__Model Top 5 Drawdowns__')
+            st.write('')
+            st.dataframe(drawdowns_df)
 
 
 
